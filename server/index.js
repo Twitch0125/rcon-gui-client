163e40d510cbec5e24f0806b17294439e6bfbc3a
Kaleb <twitcherc@gmail.com>
2020-01-12 00:19:34 -0700

    i have no idea what I'm doing

--- server/index.js
+++ server/index.js
@@ -1,42 +1,87 @@
-'use strict'
-const path = require('path')
-const consola = require('consola')
-const feathers = require('@feathersjs/feathers')
-const express = require('@feathersjs/express')
-const Rcon = require('rcon-client').Rcon
-process.env.NODE_CONFIG_DIR = path.join(__dirname, 'config/')
+"use strict";
+require("dotenv").config();
+const path = require("path");
+const consola = require("consola");
+const feathers = require("@feathersjs/feathers");
+const express = require("@feathersjs/express");
+const socketio = require("@feathersjs/socketio");
+const Rcon = require("rcon");
+process.env.NODE_CONFIG_DIR = path.join(__dirname, "config/");
 
-async function start () {
-  const app = express(feathers())
+class CommandService {
+  constructor(conn) {
+    this.conn = conn;
+    conn
+      .on("auth", function() {
+        console.log("Authed!");
+      })
+      .on("response", function(str) {
+        console.log("Got response: " + str);
+      })
+      .on("error", function(err) {
+        console.log("Got Error", err);
+      })
+      .on("end", function() {
+        console.log("Socket closed!");
+        process.exit();
+      });
 
-  const { Nuxt, Builder } = require('nuxt')
-
-  // Setup nuxt.js
-  const config = require('../nuxt.config.js')
-  config.rootDir = path.resolve(__dirname, '..')
-  config.dev = process.env.NODE_ENV !== 'production'
-
-  const nuxt = new Nuxt(config)
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  } else {
-    await nuxt.ready()
+    conn.connect();
   }
 
-  const configuration = require('@feathersjs/configuration')
-  app.configure(configuration()).use(nuxt.render)
+  async send(command) {
+    this.conn.send(command);
+  }
+}
 
-  const host = app.get('host')
-  const port = app.get('port')
-  const rconConnection = await Rcon.connect({host: process.env.RCON_HOST, port: process.env.RCON_PORT, password: process.env.RCON_PASSWORD})
+async function start() {
+  const app = express(feathers());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.configure(express.rest());
+  app.configure(socketio());
+  app.use(
+    "/commands",
+    new CommandService(
+      new Rcon(
+        process.env.RCON_HOST,
+        process.env.RCON_PORT,
+        process.env.RCON_PASSWORD
+      )
+    )
+  );
 
-  app.listen(port)
+  const { Nuxt, Builder } = require("nuxt");
+
+  // Setup nuxt.js
+  const config = require("../nuxt.config.js");
+  config.rootDir = path.resolve(__dirname, "..");
+  config.dev = process.env.NODE_ENV !== "production";
+
+  const nuxt = new Nuxt(config);
+  if (config.dev) {
+    const builder = new Builder(nuxt);
+    await builder.build();
+  } else {
+    await nuxt.ready();
+  }
+
+  const configuration = require("@feathersjs/configuration");
+  app.configure(configuration()).use(nuxt.render);
+
+  const host = app.get("host");
+  const port = app.get("port");
+
+  app.listen(port);
+
+  app.service('commands').create({
+    data: 'this is a response'
+  })
 
   consola.ready({
     message: `Feathers application started on ${host}:${port}`,
     badge: true
-  })
+  });
 }
 
-start()
+start();
